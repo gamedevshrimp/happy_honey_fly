9e225d294d9b1fe60ea7d210c2d7998625f80429
Evgenii Starostin <pareylook@gmail.com>
2023-01-13 21:30:36 +0100

    add base obstacle

--- src/BeeHero/BeeHero.gd
+++ src/BeeHero/BeeHero.gd
@@ -3,9 +3,10 @@ extends KinematicBody2D
 const GRAVITY  = 55000
 var velocity: = Vector2.DOWN
 var move: = 0.0
-var up_power: = 3
+var up_power: = 1.2
 
 func _physics_process(delta: float) -> void:
+	position.x = 570
 	velocity.y += GRAVITY
 	if (Input.is_action_pressed("move_up")):
 		velocity.y *= -up_power
