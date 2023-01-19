dc7af275f07223331c0b68b890e05fd9a9373e42
Evgenii Starostin <pareylook@gmail.com>
2023-01-19 20:26:02 +0100

    add transition

--- src/UI/MainMenu.gd
+++ src/UI/MainMenu.gd
@@ -1,4 +1,7 @@
 extends CanvasLayer
 
+onready var lvl_manager : SwitchLVLManager = get_node("/root/SwitchLevelManager")
+
 func _on_MainMenu_pressed() -> void:
-	get_tree().change_scene("res://GameLevel.tscn")
+	lvl_manager.scene_transition("res://src/GameLevel/GameLevel.tscn")
+#	get_tree().change_scene("res://GameLevel.tscn")
--- src/UI/PauseMenu.gd
+++ src/UI/PauseMenu.gd
@@ -1,14 +1,14 @@
 extends CanvasLayer
 
+onready var lvl_manager : SwitchLVLManager = get_node("/root/SwitchLevelManager")
+
 func _ready() -> void:
 	get_tree().paused = true
 
 
 func _on_MainMenu_pressed() -> void:
-	get_tree().paused = false
-	get_tree().change_scene("res://src/UI/MainMenu.tscn")
+	lvl_manager.scene_transition("res://src/UI/MainMenu.tscn")
 
 
 func _on_ButtonReset_pressed() -> void:
-	get_parent().get_tree().reload_current_scene()
-	get_tree().paused = false
+	lvl_manager.scene_transition("res://src/GameLevel/GameLevel.tscn")
