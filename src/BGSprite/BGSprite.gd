6ab8ffcc9b989de746e60fd8c763d12cfff3023f
Evgenii Starostin <pareylook@gmail.com>
2023-01-20 15:12:30 +0100

    add animation to pause menu and fix bg gap on mobile

--- src/BGSprite/BGSprite.gd
+++ src/BGSprite/BGSprite.gd
@@ -9,5 +9,5 @@ onready var bee = get_node(path_to_bee)
 func _physics_process(delta: float) -> void:
 	if (bee.position.y < position.y - (Y_SIZE / 2)):
 		position.y -= Y_SIZE * 2
-	elif (bee.position.y > position.y + (Y_SIZE * 1.7)):
+	elif (bee.position.y > position.y + (Y_SIZE * 1.5)):
 		position.y += Y_SIZE * 2
