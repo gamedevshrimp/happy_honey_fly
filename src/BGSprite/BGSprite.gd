e18756d62eb81e89b215f40a0ff94c317d838d5c
Evgenii Starostin <pareylook@gmail.com>
2023-01-22 10:43:17 +0100

    fix assert in BGSPrite

--- src/BGSprite/BGSprite.gd
+++ src/BGSprite/BGSprite.gd
@@ -4,10 +4,14 @@ const X_SIZE: = 1125
 const Y_SIZE: = 2436
 
 export (NodePath) var path_to_bee
-onready var bee = get_node(path_to_bee)
+var bee
+
+func _ready() -> void:
+	if path_to_bee:
+		bee = get_node(path_to_bee)
 
 func _physics_process(delta: float) -> void:
-	if path_to_bee:
+	if bee:
 		if (bee.position.y < position.y - (Y_SIZE / 2)):
 			position.y -= Y_SIZE * 2
 		elif (bee.position.y > position.y + (Y_SIZE * 1.5)):
